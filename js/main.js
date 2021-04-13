420d2d6d0d99ec295e836c73676c4013db9dbe48
lequyet2001 <lequyet840500@gmail.com>
2021-04-14 00:27:24 +0700

    update popup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    var modal = document.querySelector('#modal'); 
+    setTimeout(()=>{
+        modal.classList.add('show-modal')
+    },5000)
+})
