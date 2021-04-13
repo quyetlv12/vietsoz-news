9dc3cdd27e9be143818f5ea79639dba1c7d94cd3
lequyet2001 <lequyet840500@gmail.com>
2021-04-14 01:08:42 +0700

    update popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+var modal = document.querySelector('#modal');
+const btnClosePopup = document.querySelector('#btn-close')
+
+btnClosePopup.addEventListener('click', ()=>{
+    modal.classList.remove('show-modal');
+})
+
+
+const footer = document.querySelector('#footer');
+
 
 
 
@@ -6,4 +16,20 @@ window.addEventListener("DOMContentLoaded", ()=>{
     setTimeout(()=>{
         modal.classList.add('show-modal')
     },5000)
+})
+
+
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    e.returnValue = '';
+});
+
+window.addEventListener('scroll',() =>{
+    let footer = document.querySelector('#footer')
+    var y = window.scrollY;
+    if (y >= 1300) {
+        modal.classList.add('show-modal')
+    } else {
+        modal.classList.remove('show-modal')
+    }
 })
